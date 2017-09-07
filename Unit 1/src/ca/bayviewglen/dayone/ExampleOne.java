7f71a201f5016e45d974ce5afdad1416c3dcbc78
Alex-Alexiev <aalexiev@bayviewglen.ca>
2017-09-07 14:01:43 -0400

    day one

---
+++ Unit 1/src/ca/bayviewglen/dayone/ExampleOne.java
@@ -0,0 +1,15 @@
+package ca.bayviewglen.dayone;
+
+public class ExampleOne {
+
+	//every class does not need a main method
+	
+	public static void main(String[] args) { 
+		
+		System.out.println("It is my birthday"); //println starts new line
+		System.out.print("It is not my birthday");
+		System.out.print("It isss"); //prints to the end of previous line
+
+	}
+
+}
---
+++ Unit 1/src/ca/bayviewglen/dayone/ExampleTwo.java
@@ -0,0 +1,20 @@
+package ca.bayviewglen.dayone;
+
+public class ExampleTwo {
+
+	public static void main(String[] args) {
+		
+		/*
+		\" is an escape sequence for "
+		\t is an escape sequence for the tab
+		\n is an escape sequence for a new line
+		\\ is an escape sequence for a tab
+		*/
+		
+		System.out.println("My favourite word is \"nutmeg\"");
+		System.out.println("This is a backslash \\");
+		System.out.println("This is a new line \nhello there");
+
+	}
+
+}
