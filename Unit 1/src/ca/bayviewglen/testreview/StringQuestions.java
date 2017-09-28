c020bc11dd52a652b3ad62c6d9a273766ca227aa
Alex-Alexiev <aalexiev@bayviewglen.ca>
2017-09-27 23:16:47 -0400

    s

--- Unit 1/src/ca/bayviewglen/testreview/StringQuestions.java
+++ Unit 1/src/ca/bayviewglen/testreview/StringQuestions.java
@@ -4,6 +4,7 @@ public class StringQuestions {
 
 	public static void main(String[] args) {
 		
+
 		String s = "heat";
 		System.out.println(s.substring(s.length()/2-1, s.length()/2+2));
 		System.out.println(s.substring(s.length()/2));
