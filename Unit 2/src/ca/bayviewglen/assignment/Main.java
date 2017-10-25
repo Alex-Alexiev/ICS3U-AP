e9d55621012988ef1ef8ff3e1a1660851f12f7ef
Alex-Alexiev <aalexiev@bayviewglen.ca>
2017-10-24 21:55:40 -0400

    magic number fix

--- Unit 2/src/ca/bayviewglen/assignment/Main.java
+++ Unit 2/src/ca/bayviewglen/assignment/Main.java
@@ -1,3 +1,8 @@
+/*
+ * Clarfications
+ * 
+ */
+
 package ca.bayviewglen.assignment;
 
 import java.text.DecimalFormat;
@@ -11,7 +16,8 @@ public class Main {
 	private static String splitOneString, splitTwoString, splitThreeString, totalString;
 	private static String table = String.format("%32s%20s%20s%20s%20s\n", "Runner Name", "Split One Time", "Split Two Time", "Split Three Time", "Total Time");
 	private static double mileOneTimeDouble, mileTwoTimeDouble, fiveKmTimeDouble;
-
+	private static final int SECONDS_IN_MINUTE = 60;
+	
 	public static void main(String[] args) {
 		evaluateRunner();
 		evaluateRunner();
@@ -89,20 +95,20 @@ public class Main {
 	 *into a double which stores the number of seconds.
 	 */
 	private static double stringToTime (String time) {
-		double seconds = (Double.parseDouble(time.substring(0, time.indexOf(":"))))*60;
+		double seconds = (Double.parseDouble(time.substring(0, time.indexOf(":"))))*SECONDS_IN_MINUTE;
 		seconds += Double.parseDouble(time.substring(time.indexOf(":")+1));
 		return seconds;
 	}
 
 	/*
 	 *This method uses the DecimalFormat class
-	 *to convert the time as a double
+	 *to convert the time as a double of seconds
 	 *into a String with the format mm:ss.sss
 	 */
 	private static String timeToString (double time) {
-		int minutes = (int) (time/60);
+		int minutes = (int) (time/SECONDS_IN_MINUTE);
 		DecimalFormat secondsFormat = new DecimalFormat("00.000");
-		String seconds = secondsFormat.format(time%60);
+		String seconds = secondsFormat.format(time%60); 
 		return minutes + ":" + seconds;
 	}
 
