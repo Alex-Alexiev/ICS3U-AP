3b5ff181b1868ebac322bf47f4233d82bea3eae4
Alex-Alexiev <aalexiev@bayviewglen.ca>
2018-03-11 13:37:52 -0400

    Merge Sort Final

--- Unit 6/src/MergeSort.java
+++ Unit 6/src/MergeSort.java
@@ -2,25 +2,25 @@
 public class MergeSort {
 
 	public static void main(String[] args) {
-		int[] arr = {5,4,3,2,1};
+		int[] arr = { 5, 4, 3, 2, 1 };
 		int[] sorted = mergeSort(arr, 0, arr.length);
 		for (int n : sorted) {
 			System.out.println(n);
 		}
 	}
-	
+
 	public static int[] mergeSort(int[] arr, int startIndex, int endIndex) {
-		if (endIndex-startIndex <= 1) {
-			return new int[] {arr[startIndex]};
+		if (endIndex - startIndex <= 1) {
+			return new int[] { arr[startIndex] };
 		}
-		int midIndex = (startIndex+endIndex)/2;	
+		int midIndex = (startIndex + endIndex) / 2;
 		int[] a = mergeSort(arr, startIndex, midIndex);
 		int[] b = mergeSort(arr, midIndex, endIndex);
 		return combine(a, b);
 	}
 
 	public static int[] combine(int[] arr1, int[] arr2) {
-		int[] comb = new int[arr1.length+arr2.length];
+		int[] comb = new int[arr1.length + arr2.length];
 		int k = 0;
 		int i = 0;
 		int j = 0;
@@ -28,27 +28,24 @@ public class MergeSort {
 			if (arr1[i] <= arr2[j]) {
 				comb[k] = arr1[i];
 				i++;
-			}
-			else {
+			} else {
 				comb[k] = arr2[j];
 				j++;
 			}
 			k++;
 		}
-		if (i >= arr1.length) {
-			while (j < arr2.length) {
-				comb[k] = arr2[j];
-				k++;
-				j++;
-			}
+		while (j < arr2.length) {
+			comb[k] = arr2[j];
+			k++;
+			j++;
 		}
-		else {
-			while (i < arr1.length) {
-				comb[k] = arr1[i];
-				i++;
-				k++;
-			}
+		while (i < arr1.length) {
+			comb[k] = arr1[i];
+			i++;
+			k++;
 		}
 		return comb;
+
 	}
+
 }
